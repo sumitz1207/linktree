ad58371780a84b9d373902bb1d9cc99ea3e81fc3
Sumit Patel <sumitz1207@gmail.com>
2023-05-08 21:59:27 -0400

    Add files via upload

---
+++ Map1/MapView.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import Map from "./Map.jsx";
+
+function MapView(props){
+    return(
+        <section>
+            <Map />
+        </section>
+    );
+}
+
+export default MapView;
